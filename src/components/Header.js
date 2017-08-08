9b8ed16fa8156028b439c90ee6a2d43c1bca1629
ldevernay <boblehamster@yahoo.fr>
2017-08-08 10:19:12 +0200

    modify header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,10 @@
 import React from 'react';
-import logo from '../assets/fcc-logo-white.png';
+import logo from '../assets/logosimplon2.png';
 
 const Header = () =>
   (<nav className='header'>
-    <a className='header-brand' href='https://www.freecodecamp.org/'>
-      <img alt='freeCodeCamp logo' src={logo} />
-      <i
-        aria-hidden='true'
-        className='header-logo fa fa-free-code-camp fa-2x'
-      />
-    </a>
-    <ul className='header-links'>
-      <li>
-        <a
-          href='https://www.freecodecamp.org/donate'
-          rel='noopener noreferrer'
-          target='_blank'
-          >
-          Donate
-        </a>
-      </li>
-      <li>
-        <a href='https://www.freecodecamp.org/'>Return to freeCodeCamp.org</a>
-      </li>
-    </ul>
-    <a
-      href='https://www.freecodecamp.org/donate'
-      id='donate-icon'
-      rel='noopener noreferrer'
-      target='_blank'
-      >
-      <i aria-hidden='true' className='fa fa-heart' />
+    <a className='header-brand' href='https://simplon.co/'>
+      <img alt='Simplon logo' src={logo} />
     </a>
   </nav>);
 
